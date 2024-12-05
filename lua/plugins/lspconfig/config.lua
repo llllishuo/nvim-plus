6705cd8375b98514cbcbc399a6b04431ac396f30
root <root@LAPTOP-EF7ISANC.>
2024-12-05 22:22:42 +0800

    end

--- lua/plugins/lspconfig/config.lua
+++ lua/plugins/lspconfig/config.lua
@@ -19,24 +19,33 @@ return function()
 			-- Buffer local mappings.
 			-- See `:help vim.lsp.*` for documentation on any of the below functions
 			local opts = { buffer = ev.buf }
-			vim.keymap.set("n", require("custom_keys").goto_declaration, vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", require("custom_keys").goto_definition, vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", require("custom_keys").goto_references, vim.lsp.buf.references, opts)
-			vim.keymap.set("n", require("custom_keys").goto_impl, vim.lsp.buf.implementation, opts)
-			vim.keymap.set("n", require("custom_keys").lsp_rename, vim.lsp.buf.rename, opts)
-			vim.keymap.set("n", require("custom_keys").format, function()
-				vim.lsp.buf.format({ async = true })
-			end, opts)
+			vim.keymap.set("n", require("custom_keys").goto_declaration,
+				vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", require("custom_keys").goto_definition,
+				vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", require("custom_keys").goto_references,
+				vim.lsp.buf.references, opts)
+			vim.keymap.set("n", require("custom_keys").goto_impl,
+				vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", require("custom_keys").lsp_rename,
+				vim.lsp.buf.rename, opts)
+			vim.keymap.set("n", require("custom_keys").format,
+				function()
+					vim.lsp.buf.format({ async = true })
+				end, opts)
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 			vim.keymap.set("n", "<C-m>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder,
+				opts)
+			vim.keymap.set("n", "<space>wr",
+				vim.lsp.buf.remove_workspace_folder, opts)
 			vim.keymap.set("n", "<space>wl", function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			end, opts)
 			vim.keymap.set("n", "<space>m", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-		end,
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action,
+				opts)
+		end
 	})
 end
